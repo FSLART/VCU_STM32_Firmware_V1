5502dd3b78099f5ee49c9571863bcbab4723c6a9
EsTaNG9 <jpserrada@hotmail.com>
2026-09-16 21:03:43 +0100

    can 500kb

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -72,7 +72,7 @@ void MX_CAN2_Init(void)
 
   /* USER CODE END CAN2_Init 1 */
   hcan2.Instance = CAN2;
-  hcan2.Init.Prescaler = 6;
+  hcan2.Init.Prescaler = 12;
   hcan2.Init.Mode = CAN_MODE_NORMAL;
   hcan2.Init.SyncJumpWidth = CAN_SJW_2TQ;
   hcan2.Init.TimeSeg1 = CAN_BS1_7TQ;
@@ -104,7 +104,7 @@ void MX_CAN3_Init(void)
 
   /* USER CODE END CAN3_Init 1 */
   hcan3.Instance = CAN3;
-  hcan3.Init.Prescaler = 6;
+  hcan3.Init.Prescaler = 12;
   hcan3.Init.Mode = CAN_MODE_NORMAL;
   hcan3.Init.SyncJumpWidth = CAN_SJW_2TQ;
   hcan3.Init.TimeSeg1 = CAN_BS1_7TQ;
